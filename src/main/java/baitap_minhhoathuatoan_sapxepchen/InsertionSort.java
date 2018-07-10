4f7fa8f44e0cbc9c720f9c8031321c00ab178a39
1234567890 <phantiensy195@gmail.com>
2018-07-10 17:42:46 +0700

    InsertionSort bai tap minh hoa thuat toan sap xep chen sua lai

--- src/main/java/baitap_minhhoathuatoan_sapxepchen/InsertionSort.java
+++ src/main/java/baitap_minhhoathuatoan_sapxepchen/InsertionSort.java
@@ -4,31 +4,33 @@ public class InsertionSort {
     static int[] aray = {1, 5, 9, 6, 2, 4, 10, 15, 3};
 
     static void insertionSort(int[] aray) {
-        int dem = 1;
         for (int i = 1; i < aray.length; i++) {
-            boolean needNextPass = false;
-            for (int j = i; j > 0; j--) {
-                if (aray[j - 1] > aray[j]) {
-                    int temp = aray[j - 1];
-                    aray[j - 1] = aray[j];
-                    aray[j] = temp;
-                    needNextPass = true;
+            int j = i;
+            int temp = aray[i];
+            for (; j > 0; ) {
+                if (aray[j - 1] > temp) {
+                    aray[j] = aray[j - 1];
+                    j--;
+                    showOutput(aray);
+                    System.out.println();
+                } else {
+                    break;
                 }
             }
-
-            if (needNextPass == true) {
-                System.out.print("Lần sắp xếp thứ " + dem + " của mảng ta được: ");
-                for (int j : aray) {
-                    System.out.print(j + "  ");
-                }
-                System.out.println();
-                dem++;
-            }
+            aray[j] = temp;
         }
     }
 
+    static void showOutput(int[] aray) {
+        for (int i = 0; i < aray.length; i++) {
+            System.out.print(aray[i] + "\t");
+        }
+    }
+
+
     public static void main(String[] args) {
         insertionSort(aray);
-        System.out.println("Array may be sorted and next pass not needed");
+        showOutput(aray);
+        System.out.println("\n" + "Array may be sorted and next pass not needed");
     }
 }
